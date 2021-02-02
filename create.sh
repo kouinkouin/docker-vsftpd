205c308467490557c2fc7c58bef373332348dc70
Mike Hummel <mike.hummel@billingsolutions.de>
2021-02-02 08:34:04 +0100

    Update create.sh

--- create.sh
+++ create.sh
@@ -29,7 +29,6 @@ if [ "$1" = "test" ]; then
     -e PASV_MIN_PORT=21100 \
     -e PASV_MAX_PORT=21110 \
     -e PASV_ADDRESS=127.0.0.1 \
-    -e LOG_STDOUT=YES \
     -p 20:20 \
     -p 21:21 \
     -p 21100-21110:21100-21110 \
